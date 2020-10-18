3f430fec1ad570ebda71249ee48d045788d3936b
horler408 <horler.web@gmail.com>
2020-10-18 22:20:45 +0100

    Initial completion

--- js/app.js
+++ js/app.js
@@ -15,24 +15,20 @@ const search = document.getElementById('search');
 const select = document.getElementById('select');
 
 
-//Date
-let date = new Date().getTime()
-console.log(date);
-
 const cors = 'https://cors-anywhere.herokuapp.com/'
 let BASE_URL = `${cors}https://jobs.github.com/positions.json?markdown=true`
 
 // form.addEventListener('submit', (e) => {
 //   e.preventDefault()
-//   BASE_URL = `${cors}https://jobs.github.com/positions.json?markdown=true&location=${search.value}`
+//   BASE_URL = `${cors}https://jobs.github.com/positions.json?page=1&search=code`
 // })
 
 fetch(BASE_URL)
 .then(res => res.json())
 .then(data => {
     console.log(data);
-    let jobs = data.slice(0, 10)
-    //let jobs = data
+    //let jobs = data.slice(0, 10)
+    let jobs = data
 
     displayItems(jobs)
     filterSelect(jobs)
@@ -48,7 +44,7 @@ let displayItems = (items) => {
         <div class="left-details">
           <div class="top"><span class="company-name">${item.company}</span></div>
           <div class="middle">${item.title}</div>
-          <div class="bottom"><span class="time"> ${new Date(item.created_at).toLocaleDateString()}</span><span class="type">. ${item.type} </span><span class="location">. ${item.location}</span></div>
+          <div class="bottom"><span class="time"> ${getDays(item.created_at)}</span><span class="type">. ${item.type} </span><span class="location">. ${item.location}</span></div>
           <div class="details">
             <button type="button" class="detail-button">Details...</button>
             <div class="detail-text"><p>${item.description}</p></div>
@@ -105,7 +101,7 @@ var filterSearch = (searchItems) => {
   form.addEventListener("submit", (e) => {
     const location = e.currentTarget.value.toLowerCase();
     const jobLocation = searchItems.filter((searchItem) => {
-      if (searchItem.location.toLowerCase() === location) {
+      if (searchItem.location.toLowerCase().contains(location)) {
         return searchItem;
       }
     });
@@ -120,9 +116,17 @@ var filterSearch = (searchItems) => {
 };
 
 var getDays = (postDate) => {
-  const today = new Date().toLocaleDateString()
-  const date = new Date(postDate).toLocaleDateString()
-  return today - date
+  const today = new Date()
+  const date = new Date(postDate).getTime()
+  const diff = today - date
+  const oneDay = 24 * 60 * 60 * 1000;
+  let days = Math.floor(diff / oneDay)
+  if(days > 7) {
+    const week = Math.floor(days / 7)
+    return `${week}w ago`;
+  }else {
+    return `${days}d ago `;
+  }
 }
 
 // Preloader
--- js/app_2.js
+++ js/app_2.js
@@ -13,8 +13,8 @@ fetch(BASE_URL)
 .then(res => res.json())
 .then(data => {
     console.log(data);
-    let jobs = data.slice(0, 10)
-    //let jobs = data
+    //let jobs = data.slice(0, 10)
+    let jobs = data
 
     displayItems(jobs)
     filterSelect(jobs)
@@ -30,7 +30,7 @@ let displayItems = (items) => {
         <div class="left-details">
           <div class="top"><span class="company-name">${item.company}</span></div>
           <div class="middle">${item.title}</div>
-          <div class="bottom"><span class="time"> ${new Date(item.created_at).toLocaleDateString()}</span><span class="type">. ${item.type} </span><span class="location">. ${item.location}</span></div>
+          <div class="bottom"><span class="time"> ${getDays(item.created_at)}</span><span class="type">. ${item.type} </span><span class="location">. ${item.location}</span></div>
           <div class="details">
             <button type="button" class="detail-button">Details...</button>
             <div class="detail-text"><p>${item.description}</p></div>
@@ -102,9 +102,17 @@ var filterSearch = (searchItems) => {
 };
 
 var getDays = (postDate) => {
-  const today = new Date().toLocaleDateString()
-  const date = new Date(postDate).toLocaleDateString()
-  return today - date
+  const today = new Date()
+  const date = new Date(postDate).getTime()
+  const diff = today - date
+  const oneDay = 24 * 60 * 60 * 1000;
+  let days = Math.floor(diff / oneDay)
+  if(days > 7) {
+    const week = Math.floor(days / 7)
+    return `${week}w ago`;
+  }else {
+    return `${days}d ago `;
+  }
 }
 
 // Preloader
